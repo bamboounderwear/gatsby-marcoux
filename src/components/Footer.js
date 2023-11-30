560cc0eb1d18bcca4e474d1fc0cf63751a0c36e3
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-11-30 01:11:10 -0500

    white

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
-import logowhite from "../img/logo-white.png";
+import logowhite from "../img/logowhite.png";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
