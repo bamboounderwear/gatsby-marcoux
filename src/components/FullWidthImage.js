abc1c44e400a5c7a54874feeedbe1b040622e4cb
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-11-30 02:21:47 -0500

    Update FullWidthImage.js

--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -4,7 +4,7 @@ import { GatsbyImage } from "gatsby-plugin-image";
 
 export default function FullWidthImage(props) {
   const {
-    height = 700,
+    height = 400,
     img,
     title,
     subheading,
